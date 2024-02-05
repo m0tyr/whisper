c8513f025ba5a7e0fa89e155108a1a187e574b8e
Sofiane.T <sofianetlemsanipro@gmail.com>
2024-02-05 10:06:12 +0100

    home ui v1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,20 +3,33 @@
 export default function Home() {
   return (
     <div>
-      <div className="opacity-90 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
-      <img src="./test.jpg" alt="" className="rounded-3xl" />
+      <div >
+        <input type="text" name="" placeholder="Commencer un Whisper.." className="bg-navcolor  rounded-full pl-3 pr-28 pt-3 outline-none text-white " />
+        <button className="float-right right-2 mr-2 bg-white rounded-full py-2 px-4 hover:bg-slate-200 transition-all duration-150">Publier</button>
       </div>
-      <div className="opacity-90 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
-      <img src="./test.jpg" alt="" className="rounded-3xl" />
+      <div className="opacity-95 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
+        <p className="text-white py-8 px-3">Yo les gars</p>
+        <img src="./test.jpg" alt="" className="rounded-3xl" />
       </div>
-      <div className="opacity-90 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
-      <img src="./test.jpg" alt="" className="rounded-3xl" />
+      <div className="opacity-95 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
+        <p className="text-white py-8 px-3">Yo les gars</p>
+
+        <img src="./test.jpg" alt="" className="rounded-3xl" />
       </div>
-      <div className="opacity-90 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
-      <img src="./test.jpg" alt="" className="rounded-3xl" />
+      <div className="opacity-95 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
+        <p className="text-white py-8 px-3">Yo les gars</p>
+
+        <img src="./test.jpg" alt="" className="rounded-3xl" />
       </div>
-      <div className="opacity-90 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
-      <img src="./test.jpg" alt="" className="rounded-3xl" />
+      <div className="opacity-95 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
+        <p className="text-white py-8 px-3">Yo les gars</p>
+
+        <img src="./test.jpg" alt="" className="rounded-3xl" />
+      </div>
+      <div className="opacity-95 rounded-3xl hover:opacity-100 transition-all duration-300 py-12">
+        <p className="text-white py-8 px-3">Yo les gars</p>
+
+        <img src="./test.jpg" alt="" className="rounded-3xl" />
       </div>
     </div>
   )
