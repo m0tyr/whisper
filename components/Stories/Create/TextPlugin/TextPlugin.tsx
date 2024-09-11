d3a3aca90ca3c914112eea69e8a39193a77433b8
Sofiane.T <sofianetlemsanipro@gmail.com>
2024-09-11 07:56:31 +0200

    bg text fancy

--- components/Stories/Create/TextPlugin/TextPlugin.tsx
+++ components/Stories/Create/TextPlugin/TextPlugin.tsx
@@ -2,6 +2,7 @@ import LexicalContentEditable from "@/components/LexicalContentEditable/LexicalC
 import { TextInstance } from "@/lib/types/stories.types";
 import { extractElements } from "@/lib/utils";
 import { motion, useAnimation } from "framer-motion";
+import { parsePath, roundCommands, roundCorners } from "svg-round-corners";
 import Konva from "konva";
 import { Rect } from "konva/lib/shapes/Rect";
 import { Text } from "konva/lib/shapes/Text";
@@ -45,6 +46,28 @@ interface TextPluginProps {
   textCustomInstancesRef: RefObject<TextInstance[]>;
 }
 
+interface Line {
+  cx: number; // Center x-coordinate of the line
+  width: number; // Width of the line
+  lastInParagraph?: boolean; // Optional flag indicating if it's the last line in a paragraph
+}
+
+interface BackgroundShapeParams {
+  lines: Line[]; // Array of line objects
+  lineHeight: number; // Height between lines
+  width: number; // Width of the shape
+  align?: "left" | "right" | "justify" | "center"; // Alignment of lines, default is "left"
+  padding?: number; // Padding around the shape, default is 0
+  cornerRadius?: number; // Corner radius for rounded edges, default is 0
+}
+
+interface GetLineHeightParams {
+  fontLoaded: boolean; // This parameter is included but not used in the function; consider removing if unnecessary
+  fontFamily: string;
+  fontSize: number;
+  lineHeight: string | number;
+}
+
 const TextPlugin: React.FC<TextPluginProps> = ({
   isAddingNewText,
   storyProperties,
@@ -78,6 +101,106 @@ const TextPlugin: React.FC<TextPluginProps> = ({
     { variable: "var(--font-andalos)", renderedFont: "__peristiwa_df0a95" },
   ]);
 
+  function generateBackgroundShape({
+    lines,
+    lineHeight,
+    width,
+    align = "left",
+    padding = 0,
+    cornerRadius = 0,
+  }: BackgroundShapeParams): string {
+    // Set initial positions for each line based on alignment
+    lines.forEach((line, index) => {
+      // Center the line horizontally by default
+      line.cx = width / 2;
+
+      // Adjust position based on alignment
+      if (align === "right") {
+        line.cx = width - line.width / 2;
+      } else if (align === "left") {
+        line.cx = line.width / 2;
+      }
+
+      // If alignment is justify and it's not the last line in a paragraph, stretch the line to full width
+      if (align === "justify" && !line.lastInParagraph) {
+        line.width = width;
+      }
+
+      // For justified lines, adjust the center position
+      if (align === "justify") {
+        line.cx = line.width / 2;
+      }
+    });
+
+    // Initialize the path string with the starting point
+    let path = `M ${lines[0]?.cx ?? 0} ${-padding}`;
+
+    // Draw the right side of the shape
+    lines.forEach((line, index) => {
+      const { cx } = line;
+      const prevLine = lines[index - 1];
+      // Draw line to the current point based on line width comparison with the previous line
+      if (prevLine && prevLine.width > line.width) {
+        path += ` L ${cx + line.width / 2 + padding} ${
+          index * lineHeight + padding
+        }`;
+      } else {
+        path += ` L ${cx + line.width / 2 + padding} ${
+          index * lineHeight - padding
+        }`;
+      }
+
+      // Draw line to the next point
+      const nextLine = lines[index + 1];
+      if (nextLine && nextLine.width > line.width) {
+        path += ` L ${cx + line.width / 2 + padding} ${
+          (index + 1) * lineHeight - padding
+        }`;
+      } else {
+        path += ` L ${cx + line.width / 2 + padding} ${
+          (index + 1) * lineHeight + padding
+        }`;
+      }
+    });
+
+    // Draw the left side of the shape by iterating backward
+    for (let i = lines.length - 1; i >= 0; i--) {
+      const line = lines[i];
+      const { cx } = line;
+      const nextLine = lines[i + 1];
+
+      // Draw line to the current point based on line width comparison with the next line
+      if (nextLine && nextLine.width > line.width) {
+        path += ` L ${cx - line.width / 2 - padding} ${
+          (i + 1) * lineHeight - padding
+        }`;
+      } else {
+        path += ` L ${cx - line.width / 2 - padding} ${
+          (i + 1) * lineHeight + padding
+        }`;
+      }
+
+      // Draw line to the previous point
+      const prevLine = lines[i - 1];
+      if (prevLine && prevLine.width > line.width) {
+        path += ` L ${cx - line.width / 2 - padding} ${
+          i * lineHeight + padding
+        }`;
+      } else {
+        path += ` L ${cx - line.width / 2 - padding} ${
+          i * lineHeight - padding
+        }`;
+      }
+    }
+
+    // Close the path
+    path += " Z";
+
+    // Parse and round the corners of the path using external functions
+    const parsedPath = parsePath(path);
+    return roundCommands(parsedPath, cornerRadius).path;
+  }
+
   function buildCustomText(pos: {
     text: any;
     padding: { left: number; top: number; right: any; bottom: any };
@@ -91,12 +214,14 @@ const TextPlugin: React.FC<TextPluginProps> = ({
     if (stage && layer) {
       layer.destroyChildren();
 
-      // Create a temporary text node for measuring text lines (not added to the canvas)
       var textMeasure = new Konva.Text({
         text: pos.text,
-        fontFamily: "Arial",
+        fontFamily: selectedTextFont,
+        fontSize: 20,
+        fontWeight: "600",
+        textShadow: "rgba(150, 150, 150, 0.3) 0px 1px 2px",
         fill: "#ffffff",
-        visible: false, // Do not show this node, it's just for measurement
+        visible: false,
       });
 
       // Add textMeasure to the layer temporarily to measure text lines
@@ -108,7 +233,10 @@ const TextPlugin: React.FC<TextPluginProps> = ({
       textMeasure.textArr.forEach((line) => {
         var text = new Konva.Text({
           text: line.text,
-          fontFamily: "Arial",
+          fontFamily: selectedTextFont,
+          fontSize: 20,
+          fontWeight: "600",
+          textShadow: "rgba(150, 150, 150, 0.3) 0px 1px 2px",
           fill: "#ffffff",
           x: 0,
           y: top,
@@ -117,41 +245,28 @@ const TextPlugin: React.FC<TextPluginProps> = ({
         // Measure text dimensions
         var textWidth = text.width();
         var textHeight = text.height();
-
-        // Create the outer 'margin' rectangle
-        var rectMargin = new Konva.Rect({
-          x: -1 * (pos.padding.left + pos.margin.left),
-          y: top - (pos.padding.top + pos.margin.top),
-          width:
-            textWidth +
-            (pos.padding.left + pos.padding.right) +
-            (pos.margin.left + pos.margin.right),
-          height:
-            textHeight +
-            (pos.padding.top + pos.padding.bottom) +
-            (pos.margin.top + pos.margin.bottom),
-          fill: "transparent",
-        });
-        shapes.push(rectMargin);
-
-        // Create the inner 'padding' rectangle
-        var rectPadding = new Konva.Rect({
-          x: -1 * pos.padding.left,
-          y: top - pos.padding.top,
-          width: textWidth + pos.padding.left + pos.padding.right,
-          height: textHeight + pos.padding.top + pos.padding.bottom,
-          fill: "#000000ff",
-        });
-        shapes.push(rectPadding);
+        text.x((textMeasure.width() - textWidth) / 2);
         shapes.push(text);
 
         // Update the top position for the next line
-        top +=
-          textHeight +
-          pos.padding.top +
-          pos.margin.top +
-          pos.padding.bottom +
-          pos.margin.bottom;
+        top += textHeight + 2;
+      });
+
+      const path = generateBackgroundShape({
+        lines: JSON.parse(
+          JSON.stringify(textMeasure.textArr as unknown as Line[])
+        ),
+        lineHeight: textMeasure.fontSize(),
+        width: textMeasure.width(),
+        align: "center",
+        padding: pos.padding.top,
+        cornerRadius: 5,
+      });
+
+      const konvaPath = new Konva.Path({
+        data: path,
+        fill: "#000",
+        stroke: "#000",
       });
 
       // Group all shapes into a single group
@@ -198,11 +313,9 @@ const TextPlugin: React.FC<TextPluginProps> = ({
         rotationSnapTolerance: 10,
       });
 
-      transformer;
-
+      group.add(konvaPath);
       // Add each shape to the group
       shapes.forEach((shape) => group.add(shape));
-
       // Add the group to the layer
       layer.add(group);
 
@@ -278,123 +391,6 @@ const TextPlugin: React.FC<TextPluginProps> = ({
       const textWidth = calculateTextWidth(textValue, "Arial");
 
       if (textNode === null) {
-        const newText = new Konva.Text({
-          text: textValue,
-          x: storyProperties.width / 2,
-          y: storyProperties.height / 2,
-          fontSize: 16,
-          fontFamily: toRenderTextFont,
-          draggable: false,
-          width:
-            textWidth > editorRef.current?.clientWidth
-              ? editorRef.current?.clientWidth + padding * 2
-              : textWidth + padding * 2,
-          align: "center",
-          id: `${transformerInstancesRef?.current?.length}`,
-          fill: "white",
-        });
-        const transformerDependency = [] as any[];
-
-        let largestWidth = 0;
-
-        const registeredLabelFromInitialText: TextInstance = {
-          textInstances: [], // Correct initialization
-        };
-
-        newText.textArr.forEach((element) => {
-          if (element.width > largestWidth) {
-            largestWidth = element.width;
-          }
-        });
-        newText.textArr.forEach((element, index) => {
-          const xPosition = storyProperties.width / 2 - largestWidth / 2;
-          const yPosition = storyProperties.height / 2 - largestWidth / 2;
-
-          console.log(index, newText.textArr.length - 1);
-          const bgtext = new Konva.Rect({
-            x: xPosition + (largestWidth - element.width) / 2,
-            y: yPosition + index * 15.75 + padding * 2,
-            width: element.width + padding * 2,
-            height: 16,
-            cornerRadius:
-              index === newText.textArr.length - 1 ? [0, 0, 20, 20] : 6,
-            fill: "white",
-          });
-
-          const konvaText = new Konva.Text({
-            text: element.text,
-            x: 0,
-            y: 0,
-            draggable: false,
-            fontFamily: toRenderTextFont,
-            fontSize: 16,
-            fill: "black",
-            width: element.width + padding * 2,
-            align: "center",
-          });
-
-          transformerDependency.push(bgtext);
-          transformerDependency.push(konvaText);
-          /*           registeredLabelFromInitialText.textInstances.push(simpleLabel);
-           */
-        });
-        textCustomInstancesRef?.current?.push(registeredLabelFromInitialText);
-
-        whenFontIsLoaded(
-          function () {
-            newText.fontFamily(toRenderTextFont);
-          },
-          20,
-          textValue
-        );
-        const transformer = new Konva.Transformer({
-          nodes: transformerDependency,
-          anchorStroke: "#212121",
-          anchorFill: "#434343",
-          borderStroke: "#f1f1f1",
-          draggable: true,
-          anchorStyleFunc: (anchor) => {
-            anchor.cornerRadius(10);
-            anchor.fill("#2d2d2d");
-            anchor.stroke("#212121");
-            if (
-              anchor.hasName("top-center") ||
-              anchor.hasName("bottom-center")
-            ) {
-              anchor.height(6);
-              anchor.offsetY(3);
-              anchor.width(30);
-              anchor.offsetX(15);
-            }
-            if (
-              anchor.hasName("middle-left") ||
-              anchor.hasName("middle-right")
-            ) {
-              anchor.height(30);
-              anchor.offsetY(15);
-              anchor.width(6);
-              anchor.offsetX(3);
-            }
-          },
-          keepRatio: true,
-          enabledAnchors: [
-            "top-left",
-            "top-right",
-            "bottom-left",
-            "bottom-right",
-          ],
-          rotateEnabled: true,
-          resizeEnabled: true,
-          rotationSnaps: [0, 90, -90, 180, -180],
-          rotationSnapTolerance: 10,
-        });
-        console.log(transformerDependency);
-        transformerInstancesRef?.current?.push(
-          transformer as Konva.Transformer
-        );
-        layer.clear();
-        layer.add(transformer);
-        layer.draw();
         buildCustomText({
           text: makeLineBreakerMeasurer()?.join("\n"),
           x: storyProperties.width / 2,
@@ -403,7 +399,6 @@ const TextPlugin: React.FC<TextPluginProps> = ({
           padding: { left: 10, top: 10, right: 10, bottom: 10 },
           margin: { left: 5, top: 0, right: 5, bottom: 0 },
         });
-        setTextNode(newText);
         setIsInTextContext(false);
         setIsInBaseContext(true);
       } else {
@@ -576,26 +571,32 @@ const TextPlugin: React.FC<TextPluginProps> = ({
       let newElement: HTMLElement | null;
 
       if (parent) {
-        newElement = clone ?? (node as HTMLElement).cloneNode(false) as HTMLElement;
+        newElement =
+          clone ?? ((node as HTMLElement).cloneNode(false) as HTMLElement);
       } else {
         newElement = null;
       }
-  
+
       // Process child nodes
       const childNodes = node.childNodes;
-      const childNodesArray = Array.isArray(childNodes) ? childNodes : Array.from(childNodes);
-  
+      const childNodesArray = Array.isArray(childNodes)
+        ? childNodes
+        : Array.from(childNodes);
+      console.log(childNodesArray);
       for (let i = 0; i < childNodesArray.length; i++) {
         const childNode = childNodesArray[i];
+        if (childNode.nodeType !== Node.TEXT_NODE) {
+          newElement?.appendChild(childNode);
+          continue;
+        }
         const processedChildNodes = processNode(childNode, newElement);
-  
-        processedChildNodes.forEach(child => {
+        processedChildNodes.forEach((child) => {
           if (newElement) {
-            newElement.appendChild(child);
+            newElement.appendChild(child as Node);
           }
         });
       }
-  
+
       if (parent) {
         return [newElement as HTMLDivElement];
       } else {
@@ -664,13 +665,11 @@ const TextPlugin: React.FC<TextPluginProps> = ({
 
   const makeLineBreakerMeasurer = () => {
     const PlaygroundText = document.getElementById("text-playground");
-    const pElementToConvert = document.querySelector('p');
+    const pElementToConvert = document.querySelector("p");
 
     if (pElementToConvert) {
-      // Create a new <span> element to hold the combined content
       const tempSpanContent = document.createElement("span");
 
-      // Iterate over each child node of the <p> element
       pElementToConvert.childNodes.forEach((node: any) => {
         if (node.nodeType === Node.ELEMENT_NODE && node.tagName === "SPAN") {
           // If the node is a <span>, clone its text content and append it to the new <span>
@@ -692,12 +691,17 @@ const TextPlugin: React.FC<TextPluginProps> = ({
       const content = document.getElementById("temp-content");
       const torender = document.getElementById("to-render");
       // Check if the element exists
-      if (content && torender && tempSpanContent && tempSpanContent.parentNode) {
+      if (
+        content &&
+        torender &&
+        tempSpanContent &&
+        tempSpanContent.parentNode
+      ) {
         // Call the function only if content is not null
         const spans = generateTextNodes(content, torender);
         const lines = splitLines(spans);
-/*           tempSpanContent.parentNode.removeChild(tempSpanContent);
- */        return lines;
+        /*           tempSpanContent.parentNode.removeChild(tempSpanContent);
+         */ return lines;
       } else {
         console.error("Element with ID 'content' not found.");
       }
@@ -742,6 +746,7 @@ const TextPlugin: React.FC<TextPluginProps> = ({
               id="to-render"
               style={{
                 display: "inline-block",
+                width: "100%",
                 textAlign: "center",
                 verticalAlign: "baseline",
                 marginTop: "72px",
